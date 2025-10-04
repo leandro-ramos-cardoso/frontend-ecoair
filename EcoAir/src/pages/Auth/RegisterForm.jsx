c147d5d3f7e41e1ca7934dca028c89e32ccc513d
victor.lira <vrafael@indracompany.com>
2025-10-04 19:08:40 -0300

    Removido pasta do back e criado parte do register

--- EcoAir/src/pages/Auth/RegisterForm.jsx
+++ EcoAir/src/pages/Auth/RegisterForm.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Container, Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
+import { api } from "../../services/api";
 
 const RegisterForm = () => {
+     const navigate = useNavigate()
+
+  const apiUrl = import.meta.env.VITE_API_URL
+
+  const [modalAberto, setModalAberto] = useState(false)
+
+  const [usuario, setUsuario] = useState({
+    username: "",
+    email: "",
+    password: "",
+    role: "",
+  })
+
   return (
     <div>RegisterForm</div>
   )
